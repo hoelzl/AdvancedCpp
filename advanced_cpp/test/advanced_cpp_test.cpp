9464af5c6ae6d1001f2b9dffce53ea385d396e43
Matthias Hölzl <tc@xantira.com>
2021-12-20 02:29:34 +0100

    Switch test framework from Doctest to Catch2
    (Not removing Doctest for now.)

--- advanced_cpp/test/advanced_cpp_test.cpp
+++ advanced_cpp/test/advanced_cpp_test.cpp
@@ -1,59 +1,64 @@
 // Copyright (c) 2021 Dr. Matthias Hölzl.
 
 // ReSharper disable CppUnusedIncludeDirective
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "advanced_cpp.hpp"
 
-#include <cassert>
-
-#include "doctest.hpp"
+#include "catch2/catch_test_macros.hpp"
+#include <type_traits>
 
 // Only enable this code when we demonstrate the effect, since it (deliberately)
 // contains code problems that I don't want to silence for the whole project and this
 // messes up the analysis reports.
 #if 0
-#define STATIC_CHECK(expr, check) ((check) ? [&] { CHECK(expr); }() : assert(expr))
+#define MY_CHECK(expr, check) ((check) ? [&] { CHECK(expr); }() : (void)(expr))
 
-
-constexpr bool test_fake_raii_container(bool check = false, bool disable_self_checks = false)
+constexpr bool
+test_fake_raii_container(bool check = false, bool disable_self_checks = false)
 {
     auto* my_number = new int{7};
 
     auto my_container = fake_raii_container<int>{my_number};
     my_container.disable_self_checks = disable_self_checks;
 
-    STATIC_CHECK(my_container.get(), check);
-    STATIC_CHECK(*my_container.get() == 7, check);
+    MY_CHECK(my_container.get(), check);
+    MY_CHECK(*my_container.get() == 7, check);
 
     fake_raii_container<int> your_container{std::move(my_container)};
     your_container.disable_self_checks = disable_self_checks;
 
     // Deliberately using a moved-from object to check that we are correctly clearing
     // the pointer.
-    STATIC_CHECK(!my_container.get(), check);
-    STATIC_CHECK(your_container.get(), check);
-    STATIC_CHECK(*your_container.get() == 7, check);
+    MY_CHECK(!my_container.get(), check);
+    MY_CHECK(your_container.get(), check);
+    MY_CHECK(*your_container.get() == 7, check);
 
     your_container = std::move(your_container);
-    STATIC_CHECK(your_container.get(), check);
-    STATIC_CHECK(*your_container.get() == 7, check);
+    MY_CHECK(your_container.get(), check);
+    MY_CHECK(*your_container.get() == 7, check);
 
     auto his_container = fake_raii_container<int>{nullptr};
     his_container.disable_self_checks = disable_self_checks;
 
     his_container = std::move(your_container);
 
-    STATIC_CHECK(!my_container.get(), check);
-    STATIC_CHECK(!your_container.get(), check);
-    STATIC_CHECK(his_container.get(), check);
-    STATIC_CHECK(*his_container.get() == 7, check);
+    MY_CHECK(!my_container.get(), check);
+    MY_CHECK(!your_container.get(), check);
+    MY_CHECK(his_container.get(), check);
+    MY_CHECK(*his_container.get() == 7, check);
 
     return true;
 }
 
-static_assert(test_fake_raii_container(false, false));
+TEST_CASE("Fake RAII.") { test_fake_raii_container(true, false); }
 
-TEST_CASE("Fake RAII") { test_fake_raii_container(true, false); }
+TEST_CASE("Check static properties.")
+{
+    STATIC_CHECK(std::is_move_assignable_v<fake_raii_container<int>>);
+}
 
+TEST_CASE("Compile-time evaluation with STATIC_CHECK.")
+{
+    STATIC_CHECK(test_fake_raii_container(false, false));
+}
 
 #endif
