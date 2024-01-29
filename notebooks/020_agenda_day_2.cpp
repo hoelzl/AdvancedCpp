5b9f00dc1680990d51a254f2af4c467efc11fa82
Matthias Hölzl <tc@xantira.com>
2024-01-29 03:17:36 +0100

    Update specs for C++ Beginner DE

--- notebooks/020_agenda_day_2.cpp
+++ notebooks/020_agenda_day_2.cpp
@@ -1,18 +1,3 @@
-// ---
-// jupyter:
-//   jupytext:
-//     formats: cpp:light
-//     text_representation:
-//       extension: .cpp
-//       format_name: light
-//       format_version: '1.5'
-//       jupytext_version: 1.13.5
-//   kernelspec:
-//     display_name: C++17
-//     language: C++17
-//     name: xcpp17
-// ---
-
 // # Agenda (Day 2)
 //
 // - Continue tic-tac-toe example:
